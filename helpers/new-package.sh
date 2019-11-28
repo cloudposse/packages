e256534fb241ce1094d1eab5cb0a405fafb200ac
Zach Loeber <zach.loeber@gmail.com>
2019-11-28 12:52:41 -0600

    update: auto-url generation, latest version guessing

--- helpers/new-package.sh
+++ helpers/new-package.sh
@@ -2,9 +2,34 @@
 # Creates a new package based on templates
 # Author: Zachary Loeber
 
+eval `resize`
 HEIGHT=15
 WIDTH=80
 BINPATH=${BINPATH:-"tmp/build.helpers"}
+IGNORED_EXT='(.tar.gz.asc|.txt)'
+OS="${OS:-"linux"}"
+ARCH="${ARCH:-"amd64"}"
+
+function get_latest_download_urls_by_platform {
+    # Description: Scrape github releases for most recent release of a project based on:
+    # vendor, repo, os, and arch
+    # Author: Zachary Loeber
+
+    local vendorapp="${1?"Usage: $0 vendor/app"}"
+    OS="${OS:-"linux"}"
+    ARCH="${ARCH:-"amd64"}"
+	curl -s "https://api.github.com/repos/${vendorapp}/releases/latest" | \
+        jq -r --arg OS ${OS} --arg ARCH ${ARCH} \
+        '.assets[] | select(.browser_download_url | contains($OS)) | select(.browser_download_url | contains($ARCH)) | .browser_download_url'
+}
+
+function get_latest_version_by_tag {
+    # Attempt to get the latest version of a release by release tag
+    local vendorapp="${1?"Usage: $0 vendor/app"}"
+    curl -s "https://api.github.com/repos/${vendorapp}/releases/latest" | \
+        grep -oP '"tag_name": "\K(.*)(?=")' | \
+        grep -o '[[:digit:]].[[:digit:]].[[:digit:]]'
+}
 
 APP=$(whiptail --inputbox "Application Name" 8 78 "newapp" --title "Application Info" 3>&1 1>&2 2>&3)
 if [ $? -ne 0 ]; then
@@ -17,22 +42,25 @@ if [ -d "vendor/${APP}" ]; then
 fi
 
 OPTIONS=(bin "github binary"
-         tarball "github tarball"
-         custom_tarball "custom tarball")
+         tarball "github tarball")
 
 packageType=$(whiptail \
     --clear \
     --title "New Package Form" \
     --menu "Package Template Type" \
-    $HEIGHT $WIDTH 4 \
+    $LINES $COLUMNS $(( $LINES - 8 )) \
     "${OPTIONS[@]}" \
     3>&1 1>&2 2>&3)
-
 if [ $? -ne 0 ]; then
     exit 0
 fi
 
-VENDOR=$(whiptail --inputbox "Appcation Vendor" 8 78 "vendor" --title "Application Info" 3>&1 1>&2 2>&3)
+VENDOR=$(whiptail --inputbox "Github Vendor" 8 78 "vendor" --title "Application Info" 3>&1 1>&2 2>&3)
+if [ $? -ne 0 ]; then
+    exit 0
+fi
+
+REPO=$(whiptail --inputbox "Github Repo" 8 78 "repo" --title "Application Info" 3>&1 1>&2 2>&3)
 if [ $? -ne 0 ]; then
     exit 0
 fi
@@ -42,29 +70,30 @@ if [ $? -ne 0 ]; then
     exit 0
 fi
 
-VERSION=$(whiptail --inputbox "Appcation Version" 8 78 "0.0.0" --title "Application Info" 3>&1 1>&2 2>&3)
+latesturl=`get_latest_download_urls_by_platform "${VENDOR}/${REPO}"`
+latestversion=`get_latest_version_by_tag "${VENDOR}/${REPO}" | grep -o -E '[0-9]+.[0-9]+.[0-9]+'`
+if [ -z $latestversion ]; then
+    latestversion=`echo "${latesturl}" | grep -o -E '[0-9]+.[0-9]+.[0-9]+' | head -1`
+fi
+
+VERSION=$(whiptail --inputbox "Latest Github Release Version" 8 78 "${latestversion}" --title "Application Info" 3>&1 1>&2 2>&3)
 if [ $? -ne 0 ]; then
     exit 0
 fi
 
 # Construct a generic url to use based on selections
-case $packageType in
-    bin)
-        URL="\$(PACKAGE_REPO_URL)/releases/download/v\$(PACKAGE_VERSION)/${APP}-\$(PACKAGE_VERSION).\$(OS)-\$(ARCH).tar.gz"
-        ;;
-    tarball)
-        URL="\$(PACKAGE_REPO_URL)/releases/download/v\$(PACKAGE_VERSION)/${APP}-\$(PACKAGE_VERSION).\$(OS)-\$(ARCH).tar.gz"
-        ;;
-    custom_tarball)
-        URL="http://<website.name>/website.path/v\$(PACKAGE_VERSION)/${APP}-\$(PACKAGE_VERSION).\$(OS)-\$(ARCH).tar.gz"
-        ;;
-esac
-URL=$(whiptail --inputbox "Appcation URL" 8 78 "${URL}" --title "Application Info" 3>&1 1>&2 2>&3)
+PACKAGE_REPO_URL="https://github.com/${VENDOR}/${REPO}"
+latesturl=${latesturl/${PACKAGE_REPO_URL}/\$(PACKAGE_REPO_URL)}
+latesturl=${latesturl//${VERSION}/\$(PACKAGE_VERSION)}
+latesturl=${latesturl//${OS}/\$(OS)}
+latesturl=${latesturl//${ARCH}/\$(ARCH)}
+
+URL=$(whiptail --inputbox "Appcation URL" 8 78 "${latesturl}" --title "Application Info" 3>&1 1>&2 2>&3)
 if [ $? -ne 0 ]; then
-     exit 0
+    exit 0
 fi
 
-export VENDOR APP DESC VERSION URL
+export VENDOR APP DESC VERSION URL REPO
 
 echo "Template path for new application: ./vendor/${APP}"
 mkdir -p vendor/${APP}
@@ -74,14 +103,6 @@ ${BINPATH}/gomplate \
   --output-dir vendor/${APP} || rm -rf vendor/${APP}
 
 if [ -d vendor/${APP} ]; then
-## Uncomment to enable auto-update of .github/auto-label.yml
-#   LABEL="vendor/${APP}: vendor/${APP}/**"
-#   if grep -q "${LABEL}" .github/auto-label.yml; then
-#     echo "${APP} already found in .github/auto-label.yml!"
-#   else
-#     echo "${LABEL}" >> .github/auto-label.yml
-#     echo "${APP} added to .github/auto-label.yml!"
-#   fi
   echo ""
   echo "New package path: vendor/${APP}"
   echo "NOTE: Review and update all files within the new package path. Then test your app install with the following:"
