52b54964d3661db3465968ba977d702d420ab325
nitrocode <nitrocode@users.noreply.github.com>
2021-11-17 14:01:58 -0600

    Only add argocd for now

--- HomebrewFormula/argocd.rb
+++ HomebrewFormula/argocd.rb
@@ -1,28 +1,14 @@
 class Argocd < Formula
   desc "GitOps Continuous Delivery for Kubernetes"
   homepage "https://argoproj.github.io/cd"
-  # url "https://github.com/cloudposse/packages"
-  head "https://github.com/cloudposse/packages.git", branch: "homebrew"
-  version "homebrew"
-  # tag:      "v2.1.6",
-  # revision: "a346cf933e10d872eae26bff8e58c5e7ac40db25"
-  # license "Apache-2.0"
+  url "https://github.com/cloudposse/packages.git"
+  version "v2.1.6"
 
   def install
-    # ENV["INSTALL_PATH"] = HOMEBREW_PREFIX
+    ENV["INSTALL_PATH"] = "cloudposse"
     chdir "vendor/argocd" do
-      # system "sudo", "make", "install"
       system "make", "install"
+      bin.install "cloudposse/argocd"
     end
   end
-
-  # test do
-  #   assert_match "argocd controls a Argo CD server",
-  #     shell_output("#{bin}/argocd --help")
-
-  #   # Providing argocd with an empty config file returns the contexts table header
-  #   touch testpath/"argocd-config"
-  #   assert_match "CURRENT  NAME  SERVER\n",
-  #     shell_output("#{bin}/argocd context --config ./argocd-config")
-  # end
 end
